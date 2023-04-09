1aee82ef964b0298161a12dceec6469296e15b60
Tien Dung <dungbuivm@gmail.com>
2023-04-09 16:03:01 +0700

    thiết lập kết nối với database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,10 +1,18 @@
+import db from "../models/index";
+let getHomePage = async (req, res) => {
+    try {
+        let data = await db.User.findAll(); // tìm tất cả dữ liệu có trong User và gán vào biến data
 
-let getHomePage = (req, res) => {
-    return res.render('homePage.ejs')
+        return res.render("homePage.ejs", {
+            data: JSON.stringify(data),
+        });
+    } catch (e) {
+        console.log(e);
+    }
 };
 let getAboutMe = (req, res) => {
-    return res.render('./test/aboutMe.ejs')
-}
+    return res.render("./test/aboutMe.ejs");
+};
 
 module.exports = {
     getHomePage: getHomePage,
