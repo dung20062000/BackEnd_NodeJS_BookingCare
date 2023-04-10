4eb2b49ce0c13dfbe6e942ea3915b6c0039d45d2
Tien Dung <dungbuivm@gmail.com>
2023-04-10 12:23:43 +0700

    Tạo Tất Cả Models, Migrations và Seeders

--- src/migrations/migration-create-specialty.js
+++ src/migrations/migration-create-specialty.js
@@ -2,22 +2,22 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('specialties', {
+        // description: DataTypes.TEXT,
+        // image: DataTypes.STRING,
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      firstName: {
-        type: Sequelize.STRING
+      description: {
+        type: Sequelize.TEXT
       },
-      lastName: {
+      image: {
         type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
+      },  
+
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
@@ -29,6 +29,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('specialties');
   }
 };
