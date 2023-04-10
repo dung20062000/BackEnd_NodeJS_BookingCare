158dadfc1e0395094177b7b4414f5b75a50880d6
Tien Dung <dungbuivm@gmail.com>
2023-04-10 15:38:09 +0700

    Hoàn Tất Tạo Tất Cả Models, Migrations và Seeders

--- src/migrations/migration-create-user.js
+++ src/migrations/migration-create-user.js
@@ -3,13 +3,16 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Users', {
-      // id: DataTypes.STRING,
       // email: DataTypes.STRING,
+      // password: DataTypes.STRING,
       // firstName: DataTypes.STRING,
       // lastName: DataTypes.STRING,
       // address: DataTypes.STRING,
+      // phoneNumber: DataTypes.STRING,
       // gender: DataTypes.BOOLEAN,
-      // roleid: DataTypes.STRING,
+      // image: DataTypes.STRING,
+      // roleId: DataTypes.STRING,
+      // positionId: DataTypes.STRING,
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -31,13 +34,19 @@ module.exports = {
       address: {
         type: Sequelize.STRING
       },
+      phoneNumber: {
+        type: Sequelize.STRING
+      },      
       gender: {
         type: Sequelize.BOOLEAN
-      },      
-      typeRole: {
+      },
+      image: {
         type: Sequelize.STRING
       },
-      keyRole: {
+      roleId: {
+        type: Sequelize.STRING
+      },
+      positionId: {
         type: Sequelize.STRING
       },
 
