b0d68d59d2796e6333f014ad4a353f97d1db6c29
DungBui <dungbuivm@gmail.com>
2023-05-20 04:54:33 +0700

    Update

--- src/services/userService.js
+++ src/services/userService.js
@@ -23,7 +23,7 @@ let handleUserLogin = (email, password) => {
             // nếu đúng email
             if (isExist) {
                 let user = await db.User.findOne({
-                    attributes: ["email", "roleId", "password"],
+                    attributes: ["email", "roleId", "password", 'firstName', 'lastName'],
                     where: { email: email },
                     raw: true,
                 });
@@ -127,8 +127,9 @@ let createNewUser = (data) => {
                 lastName: data.lastName,
                 address: data.address,
                 phoneNumber: data.phonenumber,
-                gender: data.gender === "1" ? true : false,
+                gender: data.gender,
                 roleId: data.roleId,
+                positionId: data.positionId
             });
             resolve({
                 errCode: 0,
